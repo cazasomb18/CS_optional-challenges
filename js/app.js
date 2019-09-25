402e650a8cfa911c46be67a6ff9c5be88c8c7f9e
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-09-24 23:05:23 -0500

    files set up

---
+++ js/app.js
@@ -0,0 +1,97 @@
+console.log("Udemy JS Algorithms and Data Structures Masterclass - Section 6: Optional Challenges");
+
+//=========================================================================
+//Optional Challenges
+//=========================================================================
+
+//Frequency Counter - sameFrequency
+//Write a fucntion called sameFrequency.  Given two positive integers, findout if
+//the two numbers have the same frequency of digits.
+
+//Solution MUST have Time Complexity: O(N);
+
+//Sample input:
+	//sameFrequency(182,281) true;
+	//sameFrequency(34,14) false;
+	//sameFrequency(22,222) false;
+
+function sameFrequency(num1, num2) {
+
+}
+
+
+//Frequency Counter / Multiple Pointers - areThereDuplicates
+
+//Implement a function caslled areThereDuplicates which accepts a varaible number of arguments,
+//and checks whether there are any duplicates among the arguments passed in. You can solve this
+//using the frequency counter pattern OR the multiple pointers pattern.
+
+function areThereDuplicates() {
+
+}
+
+//Multiple Pointers - averagePair
+
+//Write a function called averagePair. Given a sorted array of integers and a target average,
+//determine if there is a pair of values in the array where the average of the pair equals
+//the target average. There may be more than one pair that matches the average target.
+
+function multiplePointers() {
+
+}
+
+//Multiple Pointers - isSubsequence
+
+//Write a function called isSubsequence which takes in two strings and checks whether the
+//characters in the first string form a subsequence of the characters in the second string.
+//In others words, the function should check whether the characters in the first string 
+//appear somewhere in the second string, without their order changing.
+
+function isSubsequence() {
+
+}
+
+//Sliding Window - maxSubarraySum
+
+//Given an array of integers and a number, write a function called maxSubarraySum, which finds
+//the maximum sum of a subarray with the length of the number passed to the function.
+
+//Note that a subarray must consist of consecutive elements from the origin array. In the first 
+//example below, [100,200,300] is a subarray of the original array, but [100,300] is not.
+
+//maxSubarraySum([100,200,300,400],2) 700
+//maxSubarraySum([1,4,2,10,23,3,1,0,20],4) 39
+//maxSubarraySum([-3,4,0,-2,6,-1],2) 5
+//maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) 5
+//maxSubarraySum([2,3],3) null
+
+//SlidingWindow - minSubArrayLen
+
+//Write a function called minSubArrayLen which accepts two parameters - an array of positive integers
+//and a positive integer.
+
+//This function should return the minimal length of a contiguous subarray of which the sum is greater
+//than or equal to the integer passed to the fucntion.  If there isn't one, return 0 instead.
+
+function minSubArrayLen() {
+
+}
+
+//Sliding Window - findLongestSubstring
+
+//Write a function called findLongestSubstring, which accepts a string and returns the length
+//of the longest substring with all distrinct characters.
+
+//findLongestSubstring('') 0
+//findLongestSubstring('rithmschool') 7
+//findLongestSubstring('thisisawesome') 6
+//findLongestSubstring('thecatinthehat') 7
+//findLongestSubstring('bbbbbb') 1
+//findLongestSubstring('longestsubstring') 8
+//findLongestSubstring('thisishowwedoit') 6
+
+function findLongestSubstring() {
+
+}
+
+
